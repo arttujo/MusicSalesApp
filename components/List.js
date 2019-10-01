98f59ae4ad7c9c1ee890a4d642e301df8815b8e0
arttujo <arttujo@gmail.com>
2019-10-01 19:46:27 +0300

    changed to only use tag based media

--- components/List.js
+++ components/List.js
@@ -1,14 +1,17 @@
-import React, { useContext, useEffect, keyExtractor, StyleSheet,useState } from "react";
+import React, {
+  useContext,
+  useEffect,
+  keyExtractor,
+  StyleSheet,
+  useState
+} from "react";
 import PropTypes from "prop-types";
 import ListItem from "./ListItem";
 import { MediaContext } from "../contexts/MediaContext";
-import {List as BaseList} from 'native-base';
+import { List as BaseList } from "native-base";
 
-
-
-
-const useFetch = (url) => {
-  const {media, setMedia} = useContext(MediaContext);
+const useFetch = url => {
+  const { media, setMedia } = useContext(MediaContext);
   const [loading, setLoading] = useState(true);
   const fetchUrl = async () => {
     const response = await fetch(url);
@@ -22,17 +25,29 @@ const useFetch = (url) => {
   return [media, loading];
 };
 
+const fetchViaTag = url => {
+  const { media, setMedia } = useContext(MediaContext);
+  const fetchUrl = async () => {
+    const response = await fetch(url);
+    const json = await response.json();
+    setMedia(json);
+  };
+  useEffect(() => {
+    fetchUrl();
+  }, []);
+  return [media];
+};
 
 const List = props => {
-  const {navigation} = props;
-  const [media, loading] = useFetch('http://media.mw.metropolia.fi/wbma/media/');
- // console.log(loading);
- // console.log('media', media);
+  const { navigation } = props;
+  const [media, loading] = fetchViaTag(
+    "http://media.mw.metropolia.fi/wbma/tags/music-sales_"
+  );
 
   return (
     <BaseList
       dataArray={media}
-      renderRow={( item ) => (
+      renderRow={item => (
         <ListItem navigation={props.navigation} singleMedia={item} />
       )}
       keyExtractor={(item, index) => index.toString()}
@@ -41,7 +56,7 @@ const List = props => {
 };
 
 List.propTypes = {
-  navigation: PropTypes.object,
+  navigation: PropTypes.object
 };
 
 export default List;
